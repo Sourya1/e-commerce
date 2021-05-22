ba23b31b4f4cfad38c52882e96c3a9808d7f1c83
Sourya Kaushik <sourya.kaushik@hashedin.com>
2021-05-22 16:38:06 +0530

    build the basics layout for main page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './HomePage.components'
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div>
+      <HomePage />
     </div>
   );
 }
