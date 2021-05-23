da430ae8fd25926935107f72fb2089e013667da1
Sourya Kaushik <sourya.kaushik@hashedin.com>
2021-05-23 08:45:50 +0530

    added the Shop Page Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Route } from 'react-router';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+import ShopPage from './pages/shop/shop.component.jsx';
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/Shop' component={ShopPage} />
+
+      </switch>
+      
     </div>
   );
 }
