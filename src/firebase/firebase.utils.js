3e5a064691a1291798f519041e97895417855d8b
Sourya Kaushik <sourya.kaushik@hashedin.com>
2021-05-23 17:49:31 +0530

    added the signin using firebase and signout function

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,22 +3,21 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD2eptespf3oeK5UH5Rz0Prawj07lXC6Rs",
-    authDomain: "e-commerce-fb937.firebaseapp.com",
-    projectId: "e-commerce-fb937",
-    storageBucket: "e-commerce-fb937.appspot.com",
-    messagingSenderId: "349314584367",
-    appId: "1:349314584367:web:dba1b188153e3384451bda"
-  };
- 
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyD2eptespf3oeK5UH5Rz0Prawj07lXC6Rs",
+  authDomain: "e-commerce-fb937.firebaseapp.com",
+  projectId: "e-commerce-fb937",
+  storageBucket: "e-commerce-fb937.appspot.com",
+  messagingSenderId: "349314584367",
+  appId: "1:349314584367:web:dba1b188153e3384451bda"
+};
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+firebase.initializeApp(firebaseConfig);
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({'prompt': 'select_account'});
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-  export default firebase;
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export default firebase;
